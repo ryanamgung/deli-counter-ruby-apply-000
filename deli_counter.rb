29e2d5c14f7801cfa927932eefe33dec7a47e3d5
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-31 13:47:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,10 @@ def line(line)
   end
 end
 
-def take_a_number(line, name)
-  line.push("#{name}")
-  puts "Welcome, #{name}. You are number #{line.length} in line."
+def take_a_number()
+  Number = 0
+  Number += 1
+  puts "Welcome, you are number #{Number} in line."
 end
 
 def now_serving(line)
